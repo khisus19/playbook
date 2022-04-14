0e04d94580485475a2115918167182b37c9877aa
Jesús Álvarez <khisus19@gmail.com>
2022-04-14 12:56:24 -0500

    Reparando el pokemon.js gracias a @brandonruizmora

--- weekly_mission_1/example9/pokemon.js
+++ weekly_mission_1/example9/pokemon.js
@@ -1,15 +1,28 @@
-/*
-  export default nos permite exportar esta clase e importara
-  
-  import MyLogger from './logger.js'
-*/
-
-export default class Logger {
+// 1.- Crea el archivo pokemon.js. 
+// 2.- Crea una clase Pokemon, exportala usando CommonJS, de tal forma que pueda usarse como se indica previamente.
+// - Crea la clase.
+// - Crea un constructor que permita recibir un nombre.
+// - Crea la función sayHello().
+// - Crea la función sayMessage().
+// - Exporta la clase para que pueda ser importada en el archivo main.js.
+class Pokemon {
     constructor(name) {
-        this.name = name
+        // this es una variable para referenciar el valor del contexto local de esta clase
+        this.name = name // Estás variables se le conocen como atributos
     }
 
-    sayMessage() {
-        console.log(`[${this.name}] ${message}`)
+    // método
+    // this.name es la variable que se guarda en el contexto local
+    // message es una variable que se le pasa al ejecutar este método
+    sayHello() {
+        console.log(`Hola yo soy un ${this.name}`)
+    }
+
+    // método
+    sayMessage(message) {
+        console.log(`${message}`)
     }
 }
+
+// Esta clase se exporta en este módulo
+module.exports = Pokemon
