6b768f97ac6d323b6ba4255ec295f7b50973dcec
Jesús Álvarez <khisus19@gmail.com>
2022-04-23 21:50:41 -0500

    Proyecto 2 - Step 3

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit Test for Spiderman class", () => {
+    test('2) Use the method getInfo()', () => {
+        // Aquí escribimos el código que queremos usar tal cual
+        // Quiero poder instanciar un objeto Spiderman con esta información
+      const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+
+      // Validamos que este código funcione de la forma esperada
+      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel sudio");
+
     });
   })
