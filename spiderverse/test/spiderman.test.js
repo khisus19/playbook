a766e4bd1d67434e34fa20b9e118cdc876a0da15
Jesús Álvarez <khisus19@gmail.com>
2022-04-24 10:26:56 -0500

    Probando el metodo de la clase spiderman success

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,9 +1,11 @@
+const Spiderman = require('./../app/spiderman')
+
 describe("Unit test for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
       const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
 
       expect(andrewGarfield.name).toBe("Spiderman Sony")
-      expect(andrewGarfield.age).toBe("31")
+      expect(andrewGarfield.age).toBe(31)
       expect(andrewGarfield.actor).toBe("Andrew Garfield")
       expect(andrewGarfield.movies).toBe(2)
       expect(andrewGarfield.studio).toBe("Sony")
