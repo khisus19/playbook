2c8bced6fa94a3e911053b6457433c7457f1346e
Jesús Álvarez <khisus19@gmail.com>
2022-04-24 11:13:25 -0500

    Segundo requerimiento terminado

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -4,13 +4,13 @@ const User = require('../../app/models/user.js');
 describe("Unit Test for User class", () => {
 
     test("Create an User Object", () => {
-        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
 
         expect(user.id).toBe(1);
         expect(user.username).toBe("carlogilmar");
         expect(user.name).toBe("Carlo");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBe();
     })
 })
