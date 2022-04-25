7a70a3e266d269afe14443c42dd9d9ac7294a236
Jesús Álvarez <khisus19@gmail.com>
2022-04-25 11:56:52 -0500

    Modificando las pruebas del requerimiento 2

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -10,7 +10,12 @@ describe("Unit Test for User class", () => {
         expect(user.username).toBe("carlogilmar");
         expect(user.name).toBe("Carlo");
         expect(user.bio).toBe("Bio");
+    })
+    
+    test("Requerimiento 2: Fecha en atributos de user", () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
         expect(user.dateCreated).not.toBeUndefined();
         expect(user.lastUpdated).not.toBe();
     })
 })
+
