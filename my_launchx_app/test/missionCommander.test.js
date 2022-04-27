14b651c9cc230e4f07b0541e86715e29b9d992e5
Jesús Álvarez <khisus19@gmail.com>
2022-04-27 11:17:48 -0500

    Agregando el proyecto de twitter

--- my_launchx_app/test/missionCommander.test.js
+++
@@ -1,9 +0,0 @@
-const MissionCommander = require('./../app/missionCommander');
-
-describe("Unit Tests for Mission Commander Class", () => {
-    test('1) Create a mission commander objet', () => {
-        const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Woopa");
-    });
-  })
-  
